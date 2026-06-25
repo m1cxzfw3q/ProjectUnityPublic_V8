31becc3ecfd734047925004a54daeab5fa15d462
m1cxzfw3q <mcxzfwq@outlook.com>
2026-06-26 01:22:26 +0800

    fix

--- main/src/unity/world/blocks/exp/turrets/OmniLiquidTurret.java
+++ main/src/unity/world/blocks/exp/turrets/OmniLiquidTurret.java
@@ -183,11 +183,46 @@ public class OmniLiquidTurret extends ExpTurret {
             return liquids.current() == liquid || liquids.currentAmount() < 0.2f;
         }
 
-        //TODO as far as I can tell, the old turret just set the bullet data to the liquid. but I could be wrong -Anuke
         @Override
-        protected void handleBullet(@Nullable Bullet bullet, float offsetX, float offsetY, float angleOffset){
-            if(bullet != null){
-                bullet.data = liquids.current();
+        protected void bullet(BulletType type, float xOffset, float yOffset, float angleOffset, Mover mover) {
+            queuedBullets --;
+
+            if(dead || (!consumeAmmoOnce && !hasAmmo())) return;
+
+            float
+                    xSpread = Mathf.range(xRand),
+                    bulletX = x + Angles.trnsx(rotation - 90, shootX + xOffset + xSpread, shootY + yOffset),
+                    bulletY = y + Angles.trnsy(rotation - 90, shootX + xOffset + xSpread, shootY + yOffset),
+                    shootAngle = rotation + angleOffset + Mathf.range(inaccuracy + type.inaccuracy);
+
+            float lifeScl = type.scaleLife ? Mathf.clamp((1 + scaleLifetimeOffset) * Mathf.dst(bulletX, bulletY, targetPos.x, targetPos.y) / type.range, minRange() / type.range, range() / type.range) : 1f;
+
+            //TODO aimX / aimY for multi shot turrets?
+            handleBullet(type.create(this, team, bulletX, bulletY, shootAngle, -1f, (1f - velocityRnd) + Mathf.random(velocityRnd), lifeScl, liquids.current(), mover, targetPos.x, targetPos.y), xOffset, yOffset, shootAngle - rotation);
+
+            (shootEffect == null ? type.shootEffect : shootEffect).at(bulletX, bulletY, rotation + angleOffset, type.hitColor);
+            (smokeEffect == null ? type.smokeEffect : smokeEffect).at(bulletX, bulletY, rotation + angleOffset, type.hitColor);
+            (type.shootSound != Sounds.none ? type.shootSound : shootSound).at(bulletX, bulletY, Mathf.random(soundPitchMin, soundPitchMax), shootSoundVolume);
+
+            ammoUseEffect.at(
+                    x - Angles.trnsx(rotation, ammoEjectBack),
+                    y - Angles.trnsy(rotation, ammoEjectBack),
+                    rotation * Mathf.sign(xOffset)
+            );
+
+            if(shake > 0){
+                Effect.shake(shake, shake, this);
+            }
+
+            curRecoil = 1f;
+            if(recoils > 0){
+                curRecoils[barrelCounter % recoils] = 1f;
+            }
+            heat = 1f;
+            totalShots++;
+
+            if(!consumeAmmoOnce){
+                useAmmo();
             }
         }
     }
